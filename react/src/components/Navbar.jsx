2168100d561fefa20411dfbf8c92209457875946
Dagin34 <dagmawinapoleon02@gmail.com>
2025-02-01 01:22:50 +0300

    Manual Rotation - DONE

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,11 +1,16 @@
+import { useNavigate } from 'react-router-dom';
+
 export const Navbar = () => {
+    const navigate = useNavigate();
     return (
         <>
             <div className="bg-[#232323] text-white w-screen px-8 py-6 flex justify-between items-center">
                 <a className="cursor-pointer hover:text-[#f22387]">Sebastopol Model</a>
-                <ul>
-                    <li className="cursor-pointer hover:text-[#f22387]">Auto-Rotate</li>
-                    <li></li>
+                <ul className='flex justify-between items-center'>
+                    <li className="cursor-pointer hover:text-[#f22387] px-2"
+                        onClick={() => { navigate("/") }}>Home</li>
+                    <li className="cursor-pointer hover:text-[#f22387] px-2"
+                        onClick={() => { navigate("/rotate") }}>Rotatable</li>
                     <li></li>
                 </ul>
             </div>
--- react/src/main.jsx
+++ react/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/Homepage.jsx'
+import Rotatable from './pages/Rotatable.jsx'
+import { Navbar } from './components/navbar.jsx'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/rotate" element={<Rotatable />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
 )
