4908edb8d2cb7402971a6e9c6eb992234043c74a
Dagin34 <dagmawinapoleon02@gmail.com>
2025-02-01 00:47:22 +0300

    Auto rotate and scaling function - DONE

--- react/src/components/Navbar.jsx
+++ react/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 export const Navbar = () => {
     return (
         <>
-            <div>
-                <p>This is the nav bar</p>
+            <div className="bg-[#232323] text-white w-screen px-8 py-6 flex justify-between items-center">
+                <a className="cursor-pointer hover:text-[#f22387]">Sebastopol Model</a>
+                <ul>
+                    <li className="cursor-pointer hover:text-[#f22387]">Auto-Rotate</li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         </>
     )
