f4cccd278c85d199523e3c44ab617f4fc204f3b0
kane0813 <kanekahsay@gmail.com>
2025-01-31 21:30:31 +0300

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./navbar"
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto text-white">
+      <h1 className="text-2xl text-center text-white ">sabastapol canon</h1>
+      <Navbar/>
+     
+    </main>
+  )
+}
+
+export default App
